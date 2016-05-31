a24ff97c461290676bca01f50309de33b8a187d1
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2016-06-01 00:55:57 +0200

    further cleanup

--- core-test/program/main.h
+++ core-test/program/main.h
@@ -3,12 +3,6 @@
 #endif
 #define _UNICODE
 
-/*
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-*/
-
 #include <core>
 #include <chrono>
 typedef core::vec4 vec4;
