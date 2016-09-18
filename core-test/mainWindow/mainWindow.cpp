bd7bedf3bee94b67f23edd47d579668aa4120fe2
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2016-09-18 18:22:39 +0200

    recompiled with all the engine changes, and fixed border rendering

--- core-test/mainWindow/mainWindow.cpp
+++ core-test/mainWindow/mainWindow.cpp
@@ -32,6 +32,6 @@ int MainWindow::onResize(const core::eventInfo &e) {
 }
 
 void MainWindow::onEndPaint(const core::eventInfo& e) {
-	core::Renderer::drawRect(getClientRect(), active?core::vec4b(0, 122, 204, 255): core::vec4b(84, 84, 84, 255), *this);
+	WindowForm::onEndPaint(e);
 	core::Renderer::drawRect(rwnd.getChildRect().expand(1), core::Color(31, 31, 31, 255), *this);
 }
