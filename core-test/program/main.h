0a61f0694448a599ca3d10763bdd69be3d692736
borna <borna@DESKTOP-61NVNBF>
2016-12-14 21:57:40 +0100

    - render moved to library

--- core-test/program/main.h
+++ core-test/program/main.h
@@ -8,23 +8,11 @@
 
 #include <core>
 #include <core-forms>
+using namespace coreTypes;
+#include <core-tracing>
 using namespace core::opengl;
-typedef core::vec4 vec4;
-typedef core::matrixf matrixf;
-typedef core::vec4s vec4s;
-typedef core::matrixs matrixs;
-typedef core::Color Color;
-typedef core::Rect Rect;
 
 //core components
-#include <model/simdMesh.h>
-#include <model/polyOctree.h>
-#include <OBVH/OBVH.h>
-#include <pointCloud/pointCloud.h>
-#include <pointCloud/pointOctree.h>
-#include <PBVH/PBVH.h>
-#include <render/view.h>
-#include <render/render.h>
 #include <render/renderTask.h>
 #include <render/msRenderTask.h>
 #include <render/subRenderTask.h>
